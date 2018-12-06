3f5cc205a999f47906b4d3ed545837ee8639f21e
sariVagshal <sari6910@gmail.com>
2018-12-06 09:21:22 +0200

    add operator bool and more

--- smartPtr.h
+++ smartPtr.h
@@ -5,12 +5,14 @@ template<typename T>
 class UniquePtr
 {
 public:
-    explicit UniquePtr(T* ptr);
+    explicit UniquePtr(T*);
     ~UniquePtr();
 
     T* operator->() const;
     T& operator*()  const;
 
+	operator bool() const;
+	
     T* get() const;
 
 private:
@@ -45,6 +47,12 @@ template<typename T>
 T& UniquePtr<T>::operator*()  const
 {
 	return *m_ptr;
+}
+
+template<typename T>
+UniquePtr<T>::operator bool() const
+{
+	return m_ptr;
 }
 
 template<typename T>
